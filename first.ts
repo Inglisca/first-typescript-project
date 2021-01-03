af75ad424cec6a0b6eb679797c9dc3aeb2d1266f
Cameron Inglis <inglisca01@gmail.com>
2021-01-03 15:04:50 -0600

    arraytesting

--- first.ts
+++ first.ts
@@ -2,5 +2,18 @@ var dogBreed: string;
 
 dogBreed = "Labrador"
 
-console.log(dogBreed)
+console.log(dogBreed);
 
+let list: number [] = [5,2,3];
+
+console.log(list[0])
+
+for (let i of list){
+    console.log(i);
+    console.log("----")
+}
+
+let stringList: string[] =["asd","asd2"];
+for (let i of stringList){
+    console.log(i);
+}
