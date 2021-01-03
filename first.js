8837afd14393e19c7ecbb7e890249697d6b72efb
Cameron Inglis <inglisca01@gmail.com>
2021-01-03 16:00:37 -0600

    if and function with race additions "cams favorite race"

--- first.js
+++ first.js
@@ -3,6 +3,7 @@ dogBreed = "Labrador";
 console.log(dogBreed);
 var list = [5, 2, 3];
 console.log(list[0]);
+//
 for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
     var i = list_1[_i];
     console.log(i);
@@ -13,11 +14,34 @@ for (var _a = 0, stringList_1 = stringList; _a < stringList_1.length; _a++) {
     var i = stringList_1[_a];
     console.log(i);
 }
-var color;
-(function (color) {
-    color[color["Red"] = 0] = "Red";
-    color[color["Green"] = 1] = "Green";
-    color[color["Blue"] = 2] = "Blue";
-})(color || (color = {}));
-var camsfavoritecolor = color.Blue;
-console.log(camsfavoritecolor);
+var Race;
+(function (Race) {
+    Race[Race["Human"] = 0] = "Human";
+    Race[Race["Dwarf"] = 1] = "Dwarf";
+    Race[Race["Gnome"] = 2] = "Gnome";
+})(Race || (Race = {}));
+var camsCharacterRace = Race.Human;
+console.log(camsCharacterRace);
+if (camsCharacterRace == Race.Human) {
+    console.log("Human");
+    console.log(camsCharacterRace);
+}
+function whatsTheRace(race) {
+    var raceToReturn;
+    switch (race) {
+        case Race.Human: {
+            raceToReturn = "Human";
+            break;
+        }
+        case Race.Dwarf: {
+            raceToReturn = "Dwarf";
+            break;
+        }
+        case Race.Gnome: {
+            raceToReturn = "Gnome";
+            break;
+        }
+    }
+    return raceToReturn;
+}
+console.log(whatsTheRace(camsCharacterRace));
--- first.ts
+++ first.ts
@@ -7,7 +7,7 @@ console.log(dogBreed);
 let list: number [] = [5,2,3];
 
 console.log(list[0]);
-
+//
 for (let i of list){
     console.log(i);
     console.log("----");
@@ -20,6 +20,33 @@ for (let i of stringList){
 
 
 enum Race {Human,Dwarf,Gnome}
-let camsCharacterrace: Race = Race.Human
+let camsCharacterRace: Race = Race.Human
 
-console.log (camsCharacterrace);
+console.log (camsCharacterRace);
+
+if(camsCharacterRace==Race.Human){
+    console.log("Human")
+    console.log(camsCharacterRace)
+}
+
+function whatsTheRace(race: Race): string {
+    let raceToReturn: string;
+
+    switch(race) {
+        case Race.Human: {
+            raceToReturn = "Human"
+            break;
+        }
+        case Race.Dwarf: {
+            raceToReturn = "Dwarf"
+            break;
+        }
+        case Race.Gnome: {
+            raceToReturn = "Gnome"
+            break;
+        }
+    }
+    return raceToReturn;
+}
+
+console.log(whatsTheRace(camsCharacterRace))
